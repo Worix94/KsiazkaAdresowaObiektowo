a1ce9612ff75822774f2e9e6bc3bc726d90eb232
Dominik Worosz <dominik.worosz.programista@gmail.com>
2021-06-22 15:33:50 +0200

    Add registered User to vector

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -7,4 +7,13 @@ class Uzytkownik
 {
     int id;
     string login, haslo;
+
+public:
+    void ustawID(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
 };
